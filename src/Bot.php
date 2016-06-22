1116d9dd4fe9ba2d1f2dae5d685b9f41b178eb0d
Aleksey Dorokhov <akeinhell@gmail.com>
2016-06-22 12:13:59 +0600

    init commit

---
+++ src/Bot.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akeinhell
+ * Date: 22.06.16
+ * Time: 12:03
+ */
+
+namespace Telegram;
+
+
+class Bot
+{
+
+}
---
+++ tests/BotTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: akeinhell
+ * Date: 22.06.16
+ * Time: 12:10
+ */
+class BotTest extends PHPUnit_Framework_TestCase
+{
+    public function testBase(){
+
+    }
+}
